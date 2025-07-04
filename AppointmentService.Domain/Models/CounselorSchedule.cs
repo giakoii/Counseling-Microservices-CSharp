90a1f581276bb621da2c509e08cc4d5d01f894de
giakoii <khoipgse172073@fpt.edu.vn>
2025-07-04 14:06:34 +0700

    [Add][Appointment] Add models appointment class

---
+++ AppointmentService.Domain/Models/CounselorSchedule.cs
@@ -0,0 +1,24 @@
+﻿namespace AppointmentService.Domain.Models;
+
+public partial class CounselorSchedule
+{
+    public Guid Id { get; set; }
+
+    public Guid CounselorId { get; set; }
+
+    public TimeOnly AvailableFrom { get; set; }
+
+    public TimeOnly AvailableTo { get; set; }
+
+    public short Weekday { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public string CreatedBy { get; set; } = null!;
+
+    public string UpdatedBy { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+}
---
+++ AppointmentService.Domain/Models/Payment.cs
@@ -0,0 +1,28 @@
+﻿namespace AppointmentService.Domain.Models;
+
+public partial class Payment
+{
+    public Guid Id { get; set; }
+
+    public Guid AppointmentId { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public string PaymentStatus { get; set; } = null!;
+
+    public string? PaymentMethod { get; set; }
+
+    public DateTime? PaidAt { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public string CreatedBy { get; set; } = null!;
+
+    public string UpdatedBy { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+
+    public virtual Appointment Appointment { get; set; } = null!;
+}
