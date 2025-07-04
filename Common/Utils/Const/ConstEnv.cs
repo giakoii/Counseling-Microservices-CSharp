455da8b01d9a3775c9d6851be683a1816d40f6b6
giakoii <khoipgse172073@fpt.edu.vn>
2025-07-04 13:58:13 +0700

    [Add][Common] Add ConstEnv class

---
+++ Common/Utils/Const/ConstEnv.cs
@@ -0,0 +1,10 @@
+namespace Common.Utils.Const;
+
+public class ConstEnv
+{
+    public const string AppointmentServiceDB = "APPOINTMENT_SERVICE_DB";
+    
+    public const string AuthServiceDB = "AUTH_SERVICE_DB";
+    
+    public const string RequestTicketServiceDB = "REQUEST_TICKET_SERVICE_DB";
+}
