168819a3c620be058af687fa0c72cbbf9c8a3594
giakoii <khoipgse172073@fpt.edu.vn>
2025-07-07 22:29:38 +0700

    [Mod][Common] Add new func FindAllAsync

--- Shared.Infrastructure/Repositories/NoSqlRepository.cs
+++ Shared.Infrastructure/Repositories/NoSqlRepository.cs
@@ -21,6 +21,16 @@ public class NoSqlRepository<TEntity> : INoSqlQueryRepository<TEntity> where TEn
     {
         var result = await _documentSession.Query<TEntity>().ToListAsync();
         return result.ToList();
+    }   
+    
+    /// <summary>
+    /// Find entities as asynchronous
+    /// </summary>
+    /// <returns></returns>
+    public async Task<List<TEntity>> FindAllAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        var result = await _documentSession.Query<TEntity>().Where(predicate).ToListAsync();
+        return result.ToList();
     }
 
     /// <summary>
